99a4757532fd09ca2a68c33c814e1c6b21131fd4
jno <jno@pisem.net>
2020-07-23 14:37:08 +0300

    provide for parallel execution of yaml-specified jobs

--- ssh_example.go
+++ ssh_example.go
@@ -2,33 +2,66 @@ package main
 
 import (
 	"flag"
-	"github.com/jn0/go-log"
+	"io/ioutil"
+	"strings"
+	"sync"
+
+	logging "github.com/jn0/go-log"
+	"gopkg.in/yaml.v2"
 )
 
 var log = logging.Root
 
-func main() {
-	log.Info("started")
-	defer log.Info("stopped")
+type Job struct {
+	Command string   `yaml:"command"`
+	Domain  string   `yaml:"domain"`
+	Hosts   []string `yaml:"hosts"`
+}
 
-	log.SetLevel(logging.DEBUG)
-	log.UsePanic(true)
+var Config struct {
+	LogLevel string
+	UseTty   bool
+	UsePanic bool
+}
 
-	use_term := false
-	host := "host.example.com"
-	cmd := "pwd"
-
-	flag.BoolVar(&use_term, "term", false, "request tty")
-	flag.Parse()
-	if flag.NArg() > 0 {
-		host = flag.Arg(0)
-	}
-	if flag.NArg() > 1 {
-		cmd = flag.Arg(1)
-	}
-
-	context := NewContext(host, use_term)
-	out := RunCommandOverSsh(context, cmd)
+func run(wg *sync.WaitGroup, context map[string]string, command string) {
+	out := RunCommandOverSsh(context, command)
 	log.Info("Got %q", out)
+	wg.Done()
+}
+
+func main() {
+	flag.BoolVar(&Config.UseTty, "term", false, "request tty")
+	flag.BoolVar(&Config.UsePanic, "log-panic", false, "use panic() for fatals")
+	flag.StringVar(&Config.LogLevel, "log-level", "INFO", "log level")
+	flag.Parse()
+	defer log.Debug("Done")
+
+	log.SetLevel(logging.LogLevelByName(strings.ToUpper(Config.LogLevel)))
+	log.UsePanic(Config.UsePanic)
+
+	wg := sync.WaitGroup{}
+	for _, arg := range flag.Args() {
+		var job Job
+		data, err := ioutil.ReadFile(arg)
+		if err != nil {
+			log.Fatal("Cannot read %q: %v", arg, err)
+		}
+		err = yaml.Unmarshal(data, &job)
+		if err != nil {
+			log.Error("Cannot process %q: %v", arg, err)
+		}
+		for _, host := range job.Hosts {
+			if job.Domain != "" {
+				host += "." + job.Domain
+			}
+			log.Info("@ %q", host)
+			context := NewContext(host, Config.UseTty)
+			wg.Add(1)
+			go run(&wg, context, job.Command)
+		}
+	}
+	log.Debug("All started")
+	wg.Wait()
 
 }
